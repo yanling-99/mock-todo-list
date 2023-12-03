9497c3297d7f533a7f3a510b73d87cc71e74f22a
yanling-99 <love87090959@gmail.com>
2023-12-03 19:11:18 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
