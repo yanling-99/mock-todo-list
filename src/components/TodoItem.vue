8d6817f668c40e150b80c5863474089419a8adca
yanling-99 <love87090959@gmail.com>
2023-12-04 09:41:42 +0800

    button for deleting a to-do item

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,3 +1,19 @@
+<template>
+  <!-- todo.isComplete 待辦是否完成 -->
+  <div :class="[todo.isComplete ? 'success' : 'error', 'todo-wrapper']">
+    <!-- todo.title 待辦名稱 -->
+    <div class="todo-title">{{ todo.title }}</div>
+    <div class="todo-icons">
+      <img src="../../public/icons/check-50.png" width="40" />
+      <img
+        src="../../public/icons/trash-48.png"
+        width="40"
+        @click="deleteItem"
+      />
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from "vue";
 
@@ -7,19 +23,12 @@ const props = defineProps({
     required: true,
   },
 });
-</script>
 
-<template>
-  <!-- todo.isComplete 代辦是否完成 -->
-  <div :class="[todo.isComplete ? 'success' : 'error', 'todo-wrapper']">
-    <!-- todo.title 待辦名稱 -->
-    <div class="todo-title">{{ todo.title }}</div>
-    <div class="todo-icons">
-      <img src="../../public/icons/check-50.png" width="40" />
-      <img src="../../public/icons/trash-48.png" width="40" />
-    </div>
-  </div>
-</template>
+const emits = defineEmits(["delete-todo"]);
+const deleteItem = () => {
+  emits("delete-todo", props.todo.id);
+};
+</script>
 
 <style scoped>
 .todo-wrapper {
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,21 @@
+<template>
+  <div class="header">
+    <h1>My Todo List</h1>
+    <img class="plus" src="../../public/icons/plus-50.png" height="40" />
+  </div>
+  <TodoItem
+    v-for="item in todos"
+    :key="item.id"
+    :todo="item"
+    @delete-todo="handleDeleteTodo"
+  />
+</template>
+
 <script setup lang="ts">
 import TodoItem from "./TodoItem.vue";
 import { ref } from "vue";
 
-const todos = [
+let todos = ref([
   {
     id: 0,
     title: "Get dressed",
@@ -11,18 +24,29 @@ const todos = [
   { id: 1, title: "Buy food", isComplete: false },
   { id: 2, title: "Eat lunch", isComplete: true },
   { id: 3, title: "Write Article", isComplete: true },
-];
+]);
+
+const handleDeleteTodo = (id: Number) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
 </script>
 
-<template>
-  <h1>My Todo List</h1>
-  <TodoItem v-for="item in todos" :key="item.id" :todo="item" />
-</template>
-
 <style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
 h1 {
   text-align: center;
-  margin-bottom: 2rem;
   font-weight: bold;
+}
+h1,
+.plus {
+  margin-bottom: 2rem;
+}
+.plus {
+  cursor: pointer;
 }
 </style>
